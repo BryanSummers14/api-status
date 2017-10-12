29e3fd84e2b177b708860a68aa796e60cbf823d2
Brian Summers <briansummers14@gmail.com>
2017-10-12 08:09:00 -0600

    added home route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port =process.env.port || 5000;
 
+app.get('/', (req, res) => {
+    res.send('welcome home');
+});
+
 app.get('/500', (req, res) => {
     res.status(500).send('Something broke!')
 });
