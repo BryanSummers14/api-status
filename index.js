d100e0fd36b6596d698209796f8f8ad6f0e49b79
Brian Summers <briansummers14@gmail.com>
2017-10-12 17:49:23 -0600

    Style error

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 
 var app = express();
 
-var port =process.env.port || 5000;
+var port = +process.env.port || 5000;
 
 app.get('/', (req, res) => {
     res.send('welcome home');
 });
 
 app.get('/500', (req, res) => {
-    res.status(500).send('Something broke!')
+    res.status(500).send('Something broke!');
 });
 
 app.listen(port, function(err) {
