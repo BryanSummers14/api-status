0508d44ba6cc49f65219316db02d57bcdeeae9d8
Brian Summers <briansummers14@gmail.com>
2017-10-12 07:57:01 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port =process.env.port || 5000;
+
+app.get('/500', (req, res) => {
+    res.status(500).send('Something broke!')
+});
+
+app.listen(port, function(err) {
+    console.log('Listening on port '+ port);
+});
