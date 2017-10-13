1b762fd59049cfc1eb819ec9e3de7f192f1c7fee
Brian Summers <briansummers14@gmail.com>
2017-10-12 18:06:50 -0600

    Updates for heroku

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var app = express();
 var port = +process.env.port || 80;
 
 app.get('/', (req, res) => {
+    res.status(200);
+    res.setHeader('Content-Type', 'text/plain');
     res.send('welcome home');
 });
 
 app.get('/500', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
     res.status(500).send('Something broke!');
 });
 
